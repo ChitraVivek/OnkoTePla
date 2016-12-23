b2cb156e87b0af3fc1944ae1a289df4fc7a0cbbb
Matthias Drescher <kingxelor@gmail.com>
2016-12-23 12:32:04 +0100

    server: fixed copy+paste-bug

--- OnkoTePla/bytePassion.OnkoTePla.Server.DataAndService/Connection/Threads/NotificationThread.cs
+++ OnkoTePla/bytePassion.OnkoTePla.Server.DataAndService/Connection/Threads/NotificationThread.cs
@@ -66,7 +66,7 @@ namespace bytePassion.OnkoTePla.Server.DataAndService.Connection.Threads
 						case NetworkMessageType.PatientUpdatedNotification:
 						{
 							var patientUpdatedNotificationObject = (PatientUpdatedNotificationObject) nextNotification;
-							outMsg = new PatientAddedNotification(patientUpdatedNotificationObject.Patient, sessionId);
+							outMsg = new PatientUpdatedNotification(patientUpdatedNotificationObject.Patient, sessionId);
 							break;
 						}
 						case NetworkMessageType.TherapyPlaceTypeAddedNotification:
