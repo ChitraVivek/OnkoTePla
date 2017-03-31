e9a1f889e0f24f450287a2cfdd9971608e2bbc1e
Matthias Drescher <kingxelor@gmail.com>
2017-03-31 16:15:34 +0200

    added DebugListener

---
+++ OnkoTePla/bytePassion.OnkoTePla.Utils/IOnkoTePlaDebugListener.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace bytePassion.OnkoTePla.Utils
+{
+	public interface IOnkoTePlaDebugListener
+	{
+		event Action<string> OnNewDebugMessage;
+	}
+}
---
+++ OnkoTePla/bytePassion.OnkoTePla.Utils/OnkoTePlaDebugListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytePassion.OnkoTePla.Utils
+{
+
+	public class OnkoTePlaDebugListener : TraceListener, IOnkoTePlaDebugListener
+	{
+		public override void Write(string message)
+		{
+			OnNewDebugMessage?.Invoke(message);
+		}
+
+		public override void WriteLine(string message)
+		{
+			OnNewDebugMessage?.Invoke(message);
+		}
+
+		public event Action<string> OnNewDebugMessage;
+	}
+}
