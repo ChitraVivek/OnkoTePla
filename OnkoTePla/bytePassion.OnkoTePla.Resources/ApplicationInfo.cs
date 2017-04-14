02cf9085d0ac703c5b4f32f44e03b2c24117c03e
Matthias Drescher <kingxelor@gmail.com>
2017-04-14 13:23:02 +0200

    updated applicationInfo

--- OnkoTePla/bytePassion.OnkoTePla.Resources/ApplicationInfo.cs
+++ OnkoTePla/bytePassion.OnkoTePla.Resources/ApplicationInfo.cs
@@ -2,7 +2,7 @@
 {
 	public static class ApplicationInfo
 	{
-		public const string ClientVersion = "1.0 Beta";
-		public const string ServerVersion = "1.0 Beta";
+		public const string ClientVersion = "1.0 Alpha";
+		public const string ServerVersion = "1.0 Alpha";
 	}
 }
